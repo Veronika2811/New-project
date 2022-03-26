49b51102b482071807b9c7dc4e83c55429737c53
Veronika2811 <smeyun@list.ru>
2022-03-26 14:52:44 +0300

    feat: add shadow in burger menu

--- shelter/assets/js/module/burger.js
+++ shelter/assets/js/module/burger.js
@@ -2,24 +2,30 @@ const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu'); 
 const menuItems = [...document.querySelectorAll('.menu__item')]
 const logo = document.querySelector('.logo'); 
+const shadow = document.querySelector('.mask-content');
+
 
 function toggleMenu() {
     burger.classList.toggle('open');
-    menu.classList.toggle('open'); 
+    menu.classList.toggle('open');
+    shadow.classList.toggle('open'); 
 
-    /*if (menu.classList.contains('open')) {
+
+    if (menu.classList.contains('open')) {
         logo.style.display = 'none';
+        
     } else {
         logo.style.display = 'block';
-    }*/
+    }
 
 };
 
 function burgerSubscribe() {
     burger.addEventListener('click', toggleMenu);
-
+    
     menuItems.forEach(el => {
         el.addEventListener('click', toggleMenu)
+        logo.style.display = 'block';
     });
 }
 
