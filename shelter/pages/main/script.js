3df56956854953f4708e00acf04e2812e9822a9f
Veronika2811 <smeyun@list.ru>
2022-06-03 02:19:19 +0300

    refactor: add spaces

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -2,8 +2,12 @@ import burgerSubscribe from "../../assets/js_modules/burger.js";
 import { modalSubscribe } from "../../assets/js_modules/popup.js";
 import slideSubscribe from "../../assets/js_modules/sliderMain.js";
 
+
 window.onload = () => {
   burgerSubscribe();
   modalSubscribe();
   slideSubscribe();
 };
+
+window.addEventListener("DOMContendLoaded", slideSubscribe)
+
