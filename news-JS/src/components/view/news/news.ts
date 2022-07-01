e1c14fa0447ce78506de12c1ab95c33dfeb9311b
Veronika2811 <smeyun@list.ru>
2022-07-01 20:25:33 +0300

    feat: add information about the presence of news and a new key

--- news-JS/src/components/view/news/news.ts
+++ news-JS/src/components/view/news/news.ts
@@ -4,36 +4,51 @@ import { Articles } from '../../interface/interface';
 
 class News {
     draw(data: Articles[]): void {
+        // if(data.length === 0) {
+        //     console.log('no');
+        // }
         const news: Articles[] = data.length >= 10 ? data.filter((_item: Articles, idx: number) => idx < 10) : data;
-        
-        const fragment = <DocumentFragment>document.createDocumentFragment();
-        const newsItemTemp = <HTMLTemplateElement>document.querySelector('#newsItemTemp');
-        
-        news.forEach((item: Articles, idx: number) => {
-            const newsClone = <DocumentFragment>newsItemTemp.content.cloneNode(true);
-        
-            if (idx % 2) (<HTMLDivElement>newsClone.querySelector('.news__item')).classList.add('alt');
-        
-            (<HTMLDivElement>newsClone.querySelector('.news__meta-photo')).style.backgroundImage = `url(${
-                item.urlToImage || './assets/news.jpeg'//'img/news_placeholder.jpg'
-            })`;
-            (<HTMLLIElement>newsClone.querySelector('.news__meta-author')).textContent = item.author || item.source.name;
-            (<HTMLLIElement>newsClone.querySelector('.news__meta-date')).textContent = item.publishedAt
-                .slice(0, 10)
-                .split('-')
-                .reverse()
-                .join('-');
-        
-            (<HTMLHeadingElement>newsClone.querySelector('.news__description-title')).textContent = item.title;
-            (<HTMLHeadingElement>newsClone.querySelector('.news__description-source')).textContent = item.source.name;
-            (<HTMLParagraphElement>newsClone.querySelector('.news__description-content')).textContent = item.description;
-            (<HTMLParagraphElement>newsClone.querySelector('.news__read-more a')).setAttribute('href', item.url);
-        
-            fragment.append(newsClone);
-        });
-        
-        (<HTMLDivElement>document.querySelector('.news')).innerHTML = '';
-        (<HTMLDivElement>document.querySelector('.news')).appendChild(fragment);
+        // console.log(news)
+
+        if (news.length === 0) {
+            (<HTMLDivElement>document.querySelector('.news')).innerHTML = '';
+            const noNews = document.createElement('p');
+            noNews.classList.add('no-news')
+            noNews.textContent = 'No news';
+            (<HTMLDivElement>document.querySelector('.news')).append(noNews);
+        } else {
+            const fragment = <DocumentFragment>document.createDocumentFragment();
+            const newsItemTemp = <HTMLTemplateElement>document.querySelector('#newsItemTemp');
+
+            news.forEach((item: Articles, idx: number) => {
+                const newsClone = <DocumentFragment>newsItemTemp.content.cloneNode(true);
+
+                if (idx % 2) (<HTMLDivElement>newsClone.querySelector('.news__item')).classList.add('alt');
+
+                (<HTMLDivElement>newsClone.querySelector('.news__meta-photo')).style.backgroundImage = `url(${
+                    item.urlToImage || './assets/news.jpeg'
+                })`;
+                (<HTMLLIElement>newsClone.querySelector('.news__meta-author')).textContent =
+                    item.author || item.source.name;
+                (<HTMLLIElement>newsClone.querySelector('.news__meta-date')).textContent = item.publishedAt
+                    .slice(0, 10)
+                    .split('-')
+                    .reverse()
+                    .join('-');
+
+                (<HTMLHeadingElement>newsClone.querySelector('.news__description-title')).textContent = item.title;
+                (<HTMLHeadingElement>newsClone.querySelector('.news__description-source')).textContent =
+                    item.source.name;
+                (<HTMLParagraphElement>newsClone.querySelector('.news__description-content')).textContent =
+                    item.description;
+                (<HTMLParagraphElement>newsClone.querySelector('.news__read-more a')).setAttribute('href', item.url);
+
+                fragment.append(newsClone);
+            });
+
+            (<HTMLDivElement>document.querySelector('.news')).innerHTML = '';
+            (<HTMLDivElement>document.querySelector('.news')).appendChild(fragment);
+        }
     }
 }
 
