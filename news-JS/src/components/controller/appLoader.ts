0f733a742fa53e2a5c41a4b623b4fe26a1eb89cb
Veronika2811 <smeyun@list.ru>
2022-07-04 13:48:01 +0300

    fix: add new key

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://nodenews.herokuapp.com/', {
-            apiKey: 'cfcf4bccd9264f96a9983de012f7cbea',
+            apiKey: '6476e5a3c8e94a3cbd13c018b8fc48e7',
         });
     }
 }
